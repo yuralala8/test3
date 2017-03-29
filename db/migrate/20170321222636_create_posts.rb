5ea8a9ad22b5b65f94537614bc6cf84ad2bf17e6
Yura Choi <yc1435@nyu.edu>
2017-03-29 21:42:27 +0000

    some change2

--- db/migrate/20170321222636_create_posts.rb
+++ db/migrate/20170321222636_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
-      t.timestamps null: false
+      t.string :title
+      t.string :content
+    t.timestamps null: false
     end
   end
 end
